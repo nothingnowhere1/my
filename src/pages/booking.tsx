ba1040415e03ae6ee15007ffefc4000946abfa6a
sweety-sweety <khaybulinas@mail.ru>
2024-04-12 23:16:27 +0300

    booking

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -102,54 +102,39 @@ const Booking = () => {
             </form>
           </aside>
         </section>
-        <section className="container2">
-          <span>
-            КОМПАНИЯ
-            <br />
-            <br />
-            О бренде
-            <br />
-            Работа
-            <br />
-            Контакты
-            <br />
-            Подарочные карты
-            <br />
-            MY CLUB
-            <br />
-          </span>
-          <span>
-            ПОМОЩЬ
-            <br />
-            <br />
-            Магазины
-            <br />
-            Оплата
-            <br />
-            Доставка
-            <br />
-            Как вернуть товар
-            <br />
-          </span>
-          <span>
-            МЫ В СОЦСЕТЯХ
-            <br />
-            <br />
-            Вконтакте
-            <br />
-            Телеграм
-            <br />
-          </span>
-          <span
-            style={{
-              marginTop: "100px",
-            }}
-          >
-            8 800 555-56-96
-            <br />
-            Ежедневно с 9:00 до 21:00 по Москве
-          </span>
-        </section>
+        <hr style={{ color: 'gray' }} />
+        <footer className="footer">
+            <div className="container2">
+                <div className="footer-column">
+                    <span>КОМПАНИЯ<br /><br />
+                        О бренде<br />
+                        Работа<br />
+                        Контакты<br />
+                        Подарочные карты<br />
+                        MY CLUB<br />
+                    </span>
+                </div>
+                <div className="footer-column">
+                    <span>ПОМОЩЬ<br /><br />
+                        Магазины<br />
+                        Оплата<br />
+                        Доставка<br />
+                        Как вернуть товар<br />
+                    </span>
+                </div>
+                <div className="footer-column">
+                    <span>МЫ В СОЦСЕТЯХ<br /><br />
+                        Вконтакте<br />
+                        Телеграм<br />
+                    </span>
+                </div>
+                <div className="footer-contact">
+                    <span>8 800 555-56-96<br />
+                        Ежедневно с 9:00 до 21:00 по Москве
+                    </span>
+                </div>
+            </div>
+        </footer>
       </main>
     </>
   );
